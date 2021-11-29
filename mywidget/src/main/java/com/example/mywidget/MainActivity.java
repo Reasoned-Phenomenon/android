caa836e15aa9c43216216bd9897877a0e8b97641
Rp <rp177@naver.com>
2021-11-29 13:10:20 +0900

    first commit 3

--- mywidget/src/main/java/com/example/mywidget/MainActivity.java
+++ mywidget/src/main/java/com/example/mywidget/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.mywidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn_plus,btn_minus,btn_multiple,btn_divide;
     EditText txtNum1, txtNum2;
     TextView tv;
 
@@ -18,22 +19,121 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
 
-        btn = findViewById(R.id.button);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_multiple = findViewById(R.id.btn_multiple);
+        btn_divide = findViewById(R.id.btn_divide);
+
         txtNum1 = findViewById(R.id.txtNum1);
         txtNum2 = findViewById(R.id.txtNum2);
-        tv = findViewById(R.id.textView4);
+        
+        tv = findViewById(R.id.txtResult); //결과창
+        
+//        //덧셈
+//        btn_plus.setOnClickListener( v-> {
+//            String strNum1 = txtNum1.getText().toString();
+//            String strNum2 = txtNum2.getText().toString();
+//
+//            int num1 = Integer.parseInt(strNum1);
+//            int num2 = Integer.parseInt(strNum2);
+//
+//            int calcNum = num1 + num2;
+//
+////            tv.setText(Integer.toString(calcNum));
+//            tv.setText(String.valueOf(calcNum));
+//
+//        });
+//
+//        //뺄셈
+//        btn_minus.setOnClickListener( v-> {
+//            String strNum1 = txtNum1.getText().toString();
+//            String strNum2 = txtNum2.getText().toString();
+//
+//            int num1 = Integer.parseInt(strNum1);
+//            int num2 = Integer.parseInt(strNum2);
+//
+//            int calcNum = num1 - num2;
+//
+//            tv.setText(String.valueOf(calcNum));
+//
+//        });
+//
+//        //곱셈
+//        btn_multiple.setOnClickListener( v-> {
+//            String strNum1 = txtNum1.getText().toString();
+//            String strNum2 = txtNum2.getText().toString();
+//
+//            int num1 = Integer.parseInt(strNum1);
+//            int num2 = Integer.parseInt(strNum2);
+//
+//            int calcNum = num1 * num2;
+//
+//            tv.setText(String.valueOf(calcNum));
+//
+//        });
+//
+//        //나눗셈
+//        btn_divide.setOnClickListener( v-> {
+//            String strNum1 = txtNum1.getText().toString();
+//            String strNum2 = txtNum2.getText().toString();
+//
+//            double num1 = Integer.parseInt(strNum1);
+//            double num2 = Integer.parseInt(strNum2);
+//
+//            double calcNum = (double) num1 / num2;
+//
+//            tv.setText(String.valueOf(calcNum));
+//
+//        });
 
-        btn.setOnClickListener( v-> {
+        //이벤트 핸들러 - 동일한 핸들러로 처리.
+        View.OnClickListener handler = v -> {
+
+            int n1 = Integer.parseInt(txtNum1.getText().toString());
+            int n2 = Integer.parseInt(txtNum2.getText().toString());
+            int result =0;
+            switch (v.getId()) {
+                case R.id.btn_plus: result = n1+n2; break;
+                case R.id.btn_minus: result = n1 - n2; break;
+                case R.id.btn_multiple: result = n1 * n2; break;
+                //case R.id.btn_divide: result = n1 / n2; break; //double 어떻게 하면 좋을지.
+            }
+
+            tv.setText(String.valueOf(result));
+
+        };
+
+        btn_plus.setOnClickListener(handler);
+        btn_minus.setOnClickListener(handler);
+        btn_multiple.setOnClickListener(handler);
+        //나눗셈
+        btn_divide.setOnClickListener( v-> {
             String strNum1 = txtNum1.getText().toString();
             String strNum2 = txtNum2.getText().toString();
 
-            int num1 = Integer.parseInt(strNum1);
-            int num2 = Integer.parseInt(strNum2);
+            double num1 = Integer.parseInt(strNum1);
+            double num2 = Integer.parseInt(strNum2);
 
-            int calcNum = num1 + num2;
+            double calcNum = (double) num1 / num2;
 
-            tv.setText(Integer.toString(calcNum));
+            tv.setText(String.valueOf(calcNum));
+
         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //1번 방법 - 상속
 //        btn.setOnClickListener(this); // implements View.OnClickListener 필요.
@@ -73,10 +173,6 @@ public class MainActivity extends AppCompatActivity {
 //            Toast.makeText(getApplicationContext(), "클릭!!!", Toast.LENGTH_LONG).show();
 //            System.out.println("클릭됨");
 //        });
-
-
-
-
 
     }
 
