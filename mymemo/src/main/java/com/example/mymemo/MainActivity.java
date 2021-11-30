7ff687663c5b1961d7312032a127a33f85314c94
Rp <rp177@naver.com>
2021-11-30 10:43:49 +0900

    1130 commit

--- mymemo/src/main/java/com/example/mymemo/MainActivity.java
+++ mymemo/src/main/java/com/example/mymemo/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.mymemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_insert, btn_read;
+    EditText et_name, et_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_insert = findViewById(R.id.btn_insert);
+        btn_read = findViewById(R.id.btn_read);
+
+        et_name = findViewById(R.id.et_name);
+        et_view = findViewById(R.id.et_view);
+
+        DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext()); //DB, table 생성
+
+        View.OnClickListener handler = v -> {
+
+            String InsertName = et_name.getText().toString();
+
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            String sqlInsert = "INSERT INTO emp " +
+                    "(NAME, age, mobile) VALUES ('" + InsertName + "',20,'010-211')";
+
+            db.execSQL(sqlInsert);
+
+            et_view.setText(InsertName);
+            et_name.setText("");
+
+        };
+
+        //db.close();
+
+        btn_insert.setOnClickListener(handler);
+
     }
 }
