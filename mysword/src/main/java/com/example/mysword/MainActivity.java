e81ab5919fa7365dc77fa081fa7f622e435fb5b3
rp177 <rphenomenon177@gmail.com>
2021-12-12 17:10:39 +0900

    1212 오후

--- mysword/src/main/java/com/example/mysword/MainActivity.java
+++ mysword/src/main/java/com/example/mysword/MainActivity.java
@@ -1,54 +1,97 @@
 package com.example.mysword;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView view_rv;
+
+    EditText et_input;
+    Button btn_send;
+
+    String str_def,inputWord;
+    ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //화면 로드시, volly 사용 -> 사진, 성함 가져오기
+        //view 선언.
+        view_rv = findViewById(R.id.view_rv);
+        et_input = findViewById(R.id.et_input);
+        btn_send = findViewById(R.id.btn_send);
+
+        //queue, gson 선언.
         RequestQueue queue = Volley.newRequestQueue(this);
         Gson gson = new Gson();
-        String inputWord = "사과";
-        String url = "https://stdict.korean.go.kr/api/search.do?key=228B1E6329BDCFE43D09A1BE5129B58B&req_type=json&q="+inputWord;
-        StringRequest request = new StringRequest(url, s->{
 
-            if (s.isEmpty()) {
-                System.out.println("없는 단어입니다");
-            } else {
-                Map<String,Object> map = gson.fromJson(s,Map.class);
-                System.out.println(((Map)((Map)((List)((Map)map.get("channel")).get("item")).get(0)).get("sense")).get("definition"));
+        //어댑터
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        view_rv.setLayoutManager(layoutManager);
+        list.add("제시어");
+        System.out.println(list);
+        view_rv.setAdapter(new MyRecycleAdapter(this,list));
+
+        //입력버튼
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String lastWord = list.get(list.size()-1);
+                String lastLetter = lastWord.substring(lastWord.length()-1);
+
+                inputWord = et_input.getText().toString();
+                String firstLetter = inputWord.substring(0,1);
+                System.out.println(firstLetter);
+                if(lastLetter.equals(firstLetter)) {
+                    String url = "https://stdict.korean.go.kr/api/search.do?key=228B1E6329BDCFE43D09A1BE5129B58B&req_type=json&q="+inputWord;
+                    StringRequest request = new StringRequest(url, s->{
+
+                        if (s.isEmpty()) {
+                            System.out.println("없는 단어입니다");
+                        } else {
+
+                            Map<String,Object> map = gson.fromJson(s,Map.class);
+                            str_def = ((Map)((Map)((List)((Map)map.get("channel")).get("item")).get(0)).get("sense")).get("definition").toString();
+
+                            //설명부분 토스트
+                            Toast.makeText(getApplicationContext(),str_def,Toast.LENGTH_SHORT).show();
+
+                            list.add(inputWord);
+                            view_rv.setAdapter(new MyRecycleAdapter(getApplicationContext(),list));
+                            ((MyRecycleAdapter)view_rv.getAdapter()).notifyDataSetChanged();
+
+                            et_input.setText("");
+                        }
+                    },e->{
+                        System.out.println("에러발생");
+                        System.out.println(e);
+                    });
+                    queue.add(request);
+                } else {
+                    System.out.println("다시 입력해주세요");
+                }
 
             }
-//            Map<String,Object> map = gson.fromJson(s,Map.class);
-//            System.out.println(map);
-//            System.out.println(map.get("channel"));
-//            System.out.println(((Map)map.get("channel")).get("item"));
-//            System.out.println(((List)((Map)map.get("channel")).get("item")).get(0));
-//            System.out.println(((List)((Map)map.get("channel")).get("item")).get(1));
-
-            //완성
-//            System.out.println(((Map)((List)((Map)map.get("channel")).get("item")).get(0)).get("sense"));
-//            System.out.println(((Map)((Map)((List)((Map)map.get("channel")).get("item")).get(0)).get("sense")).get("definition"));
-        },e->{
-            System.out.println("에러발생");
-            System.out.println(e);
         });
 
-        queue.add(request);
 
     }
 }
---
+++ mysword/src/main/java/com/example/mysword/MyRecycleAdapter.java
@@ -0,0 +1,77 @@
+package com.example.mysword;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyRecycleAdapter extends RecyclerView.Adapter<MyRecycleAdapter.MyViewHolder> {
+
+    ArrayList<String> data;
+    Context context;
+
+    public ArrayList<String> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<String> data) {
+        this.data = data;
+    }
+
+    public MyRecycleAdapter(Context context, ArrayList<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    public MyRecycleAdapter () {};
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycle_item, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        if (position%2 == 1) {
+            holder.tv_txt.setBackground(ContextCompat.getDrawable(context,R.drawable.bubble_send));
+            holder.tv_txt.setGravity(Gravity.RIGHT);
+        } else {
+            holder.tv_txt.setBackground(ContextCompat.getDrawable(context,R.drawable.bubble_receive));
+            holder.tv_txt.setGravity(Gravity.LEFT);
+        }
+
+        holder.tv_txt.setText(data.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_txt;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_txt = itemView.findViewById(R.id.tv_txt);
+        }
+
+    }
+}
